1e9feb4bfe1805f9b98344fa0bd87abc98bcd88a
kwonhoe1121 <kwonhoe1121@gmail.com>
2021-06-20 16:06:24 +0900

    add funtions in commonapi

--- algorithm/src/commonapi.h
+++ algorithm/src/commonapi.h
@@ -12,6 +12,10 @@ void right_rotate(int arr[], int s, int t);
 void right_n_rotate(int arr[], int s, int t, int n); 
 void left_rotate(int arr[], int s, int t);
 void left_n_rotate(int arr[], int s, int t, int n);
+void print_arr(int arr[], int len);
+int all_is(int arr[], int len, int num);
+int decimalToBinary(char *binary, const int len,  int decimal);
+int countBinary1(const char *binary, const int len);
 
 #endif
 
